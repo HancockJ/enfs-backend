7f4d177f31f09638e953e0d87b0eea9d876e0746
jackhancock <hancockjack99@gmail.com>
2022-08-04 18:16:35 -0400

    added in the testFrontend file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,60 +1,12 @@
 import express from 'express';
-import { infuraKey } from '../config';
 const genex = require('genex');
 const {ethers} = require("ethers");
 const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/d163401424514af5bd48d03741865114');
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: infuraKey }));
-
-indexRouter.get('/search', (req,res) => {
-    res.send('You got search')
-})
-
-
-indexRouter.get('/axiosTest', (req,res) => {
-    f().then(response => {
-        res.send(response)
-    })
-})
-
-indexRouter.get('/axiosGetTwo', (req,res) => {
-    let name = "vitalik"
-    provider.resolveName(name + ".eth").then(ens => {
-        if(ens === null){
-            console.log(name + ".eth is available!");
-            res.send(name + ".eth is available!");
-        }else{
-            console.log(name + ".eth is owned by " + ens);
-            res.send(name + ".eth is owned by " + ens)
-        }
-    })
-})
-
-indexRouter.post('/axiosGetTwo', (req,res) => {
-    let input = JSON.stringify(req.body)
-    // console.log(input)
-    let name = JSON.parse(input).name
-    checkSingleName(name).then(response => {
-        res.send(response)
-    })
-})
-
-indexRouter.post('/axiosPost', (req,res) => {
-    let input = JSON.stringify(req.body)
-    // console.log(input)
-    let name = JSON.parse(input).name
-    checkSingleName(name).then(response => {
-        res.send(response)
-    })
-})
-
-indexRouter.post('/search', (req,res) => {
-    let name = req.body.name
-   checkSingleName(name).then(response => {
-       res.send(String(response));
-   })
+indexRouter.get('/', (req,res) => {
+    res.send('You have queried the backend ENFS server')
 })
 
 indexRouter.post('/checkNames', (req,res) => {
@@ -64,25 +16,6 @@ indexRouter.post('/checkNames', (req,res) => {
         res.send(Object.fromEntries(response))
     })
 })
-
-async function f() {
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve("Axios done!"), 1000)
-    });
-    return await promise;
-}
-
-
-//curl -d '{"name":"vitalik"}' -H "Content-Type: application/json" -X POST http://localhost:3000/v1/search
-
-async function checkSingleName(name) {
-    let address = await provider.resolveName(name + ".eth");
-    if(address === null){
-        return(name + ".eth is available!");
-    }else{
-        return(name + ".eth is owned by " + address)
-    }
-}
 
 function getMatches(regexString) {
     try {
---
+++ testFrontEnd.js
@@ -0,0 +1,102 @@
+const axios = require("axios");
+const {ethers} = require("ethers");
+const genex = require("genex");
+const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/d163401424514af5bd48d03741865114');
+
+
+async function f() {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("Axios done!"), 5000)
+    });
+    return await promise;
+}
+
+// f().then(result => {
+//     console.log(result)
+// })
+//
+// axios.get('http://localhost:3000/v1/axiosGetTwo')
+//     .then(response => {
+//         console.log(response.data);
+//         console.log("SUCCESS")
+//     })
+//     .catch(error => {
+//         console.log(error)
+//         console.log("ERROR")
+//     })
+
+// axios.post('http://localhost:3000/v1/search', {name: "jack"})
+//     .then(response => {
+//         console.log(response.data);
+//         console.log("SUCCESS")
+//     })
+//     .catch(error => {
+//         console.log(error)
+//         console.log("ERROR")
+//     })
+
+
+
+axios.post('http://localhost:3000/v1/checkNames', {regex: "jack\\d"})
+    .then(response => {
+        console.log(response.data);
+        console.log("SUCCESS")
+    })
+    .catch(error => {
+        console.log(error)
+        console.log("ERROR")
+    })
+
+
+
+
+// async function getResolver(name) {
+//     return await provider.getResolver(name + ".eth");
+// }
+// async function getAddress(name) {
+//     return await provider.resolveName(name + ".eth");
+// }
+//
+// function test(name){
+//     getResolver(name).then(response => {
+//         console.log(response);
+//     })
+//     getAddress(name).then(response => {
+//         console.log(response);
+//     })
+// }
+// test("jack")
+
+//
+// async function checkNameList(nameList) {
+//     const nameMap = new Map();
+//     for(let i in nameList){
+//         nameMap.set(nameList[i], await provider.resolveName(nameList[i] + ".eth"));
+//     }
+//     return nameMap;
+// }
+//
+// checkNameList(["jack", "will", "vitalik"]).then(response => {
+//     response.forEach((value, key) => {
+//         console.log(value + " - " + key)
+//     });
+// })
+
+//
+// function getMatches(regexString) {
+//     try {
+//         const pattern = genex(regexString);
+//         if(pattern.count() < 1000){
+//             let matches = pattern.generate();
+//             let legalMatches = matches.filter(word => /^[A-Za-z\d]*$/.test(word));
+//             return legalMatches;
+//         }
+//     } catch (error) {
+//         return([regexString])
+//     }
+// }
+
+// console.log(getMatches("jack\\d"));
+
+
+
